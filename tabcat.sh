dbbc4c4548c6c908a6c422fd3d0ebf18c6b7345e
Phillip Alexander <glupmjoed@fafnir.dk>
2018-11-11 18:38:46 +0100

    Add option handling

--- tabcat.sh
+++ tabcat.sh
@@ -4,6 +4,16 @@ BROWSER="firefox"
 DELAY=.5
 TABFLAG=-"new-tab"
 
+usage="Usage: $0 [FILE]"
+
+while getopts "" opt; do
+    case ${opt} in
+        \? ) echo $usage 1>&2
+             exit 1 ;;
+    esac
+done
+shift $((OPTIND - 1))
+
 while read -r addr
 do
     $BROWSER $TABFLAG "$addr"
